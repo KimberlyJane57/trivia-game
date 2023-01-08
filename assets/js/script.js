9fe13f645468a2973be4f747ec4f0251a4296191
KimberlyJane57 <kimberlythemotherofgod@gmail.com>
2023-01-07 19:23:35 -0700

    start button starting timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
-const scorecard = document.querySelector('.scorecard')
-const timer = document.querySelector('.timer')
-const startBtn = document.querySelector('#start-btn')
-const questions = document.querySelector('.questions')
-const answerBtn = document.querySelector('.answer-btns')
-const abcdBtn = document.querySelector('#abcd-btns')
-const multipleChoice = document.querySelector('.mc-btn')
+const scorecard = document.querySelector('#scorecard');
+const timer = document.querySelector('#timer');
+const startBtn = document.querySelector('#start-btn');
+const questions = document.querySelector('.questions');
+const answerBtn = document.querySelector('.answer-btns');
+const abcdBtn = document.querySelector('#abcd-btns');
+const multipleChoice = document.querySelector('.mc-btn');
 const startTime = 60;
+const score = 100;
 const questionsArray = [
     {
-        question: 'Which house was Harry Potter almost sorted into?',
+        question: '1. Which house was Harry Potter almost sorted into?',
         option1: 'A',
         option2: 'B',
         option3: 'Slytherin',
@@ -16,7 +17,7 @@ const questionsArray = [
         answer: 3,
     },
     {
-        question: 'Which country gifted the Statue of Liberty to the US?',
+        question: '2. Which country gifted the Statue of Liberty to the US?',
         option1: 'A',
         option2: 'B',
         option3: 'C',
@@ -24,7 +25,7 @@ const questionsArray = [
         answer: 4,
     },
     {
-        question: 'What was the name of the Robin Williams film where he dresses up as an elderly British nanny?',
+        question: '3. What was the name of the Robin Williams film where he dresses up as an elderly British nanny?',
         option1: 'A',
         option2: 'B',
         option3: 'Mrs. Doubtfire',
@@ -32,7 +33,7 @@ const questionsArray = [
         answer: 3,
     },
     {
-        question: 'What is the rarest blood type?',
+        question: '4. What is the rarest blood type?',
         option1: 'AB-Negative',
         option2: 'B',
         option3: 'C',
@@ -40,7 +41,7 @@ const questionsArray = [
         answer: 1,
     },
     {
-        question: 'What sport does Cristiano Ronaldo play?',
+        question: '5. What sport does Cristiano Ronaldo play?',
         option1: 'A',
         option2: 'B',
         option3: 'Soccer/football',
@@ -48,7 +49,7 @@ const questionsArray = [
         answer: 3,
     },
     {
-        question: 'How many bones are there in the human body?',
+        question: '6. How many bones are there in the human body?',
         option1: 'A',
         option2: '206',
         option3: 'C',
@@ -56,33 +57,24 @@ const questionsArray = [
         answer: 2,
     },
     {
-        question: 'What is the name of the longest river in South America?',
+        question: '7. What is the name of the longest river in South America?',
         option1: 'A',
         option2: 'Amazon River',
         option3: 'C',
         option4: 'D',
         answer: 2,
     } 
-]
+];
+const numberOfQuestions = 7;
 
 
 startBtn.addEventListener('click', start)
 
 function start() {
-}
+    
 
+};
 
-countdownClock = '00:$(startTime)';
-
-const countdown = setInterval (()=>{
-    startTime--;
-    displayTimer(startTime);
-    if(startTime <= 0 || startTime < 1){
-        clearInterval(countdown);
-    }
-},1000)
-
-function displayTimer(second) {}
 
 function nextQuestion() {
 
