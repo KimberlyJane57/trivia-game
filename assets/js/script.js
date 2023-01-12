70b6e86e25f1faf5723fb0073a8a9f7e0df13e1b
KimberlyJane57 <kimberlythemotherofgod@gmail.com>
2023-01-11 19:48:36 -0700

    update questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,14 @@ const scorecard = document.getElementById('scorecard');
 const startBtn = document.getElementById('start-btn');
 const answerBtn = document.getElementsByClassName('answer-btns');
 const abcdBtn = document.getElementById('abcd-btns');
-const multipleCh = [
-    document.getElementById('form0'),
-    document.getElementById('form1'),
-    document.getElementById('form2'),
-    document.getElementById('form3'),
-    document.getElementById('form4'),
-    document.getElementById('form5'),
-    document.getElementById('form6')
-]
-const answers = ['C','D','C','A','C','B','B']
+const displayTrivia = document.getElementById('displayTrivia');
+const question = document.getElementById('question');
+const answerA = document.getElementById('labelA');
+const answerB = document.getElementById('labelB');
+const answerC = document.getElementById('labelC');
+const answerD = document.getElementById('labelD');
+const submitbtn = document.getElementById('submitbtn')
+
 let score = 100;
 
 const totalScore = 100;
@@ -22,30 +20,35 @@ let curentSelection = 0
 
 
 startBtn.addEventListener('click', start)
-multipleCh.forEach((answer) => {
-    answer.addEventListener('submit',nextQuestion)
-})
 
-function getCurrentSelection(){
-    let current = document.getElementById(curentSelection)
-    return current
+submitbtn.addEventListener('click', nextQuestion)
+
+function populateQuestion(){
+    question.innerHTML = questionsArray[curentSelection].question
+    console.log(answerA)
+    console.log(answerB)
+    console.log(answerC)
+    console.log(answerD)
+    answerA.innerHTML = questionsArray[curentSelection].option1
+    answerB.innerHTML = questionsArray[curentSelection].option2
+    answerC.innerHTML = questionsArray[curentSelection].option3
+    answerD.innerHTML = questionsArray[curentSelection].option4
 }
-function toggleHide(current){
-    current.classList.toggle('hide')
+function toggleHide(){
+    displayTrivia.classList.toggle('hide')
 }
 
 function start() {
     console.log ('start')
     console.log ('hide' + curentSelection)
-    let current = getCurrentSelection()
-    toggleHide(current)
+    populateQuestion()
+    toggleHide()
     countdown()
 };
 function nextQuestion(event){
     event.preventDefault()
-    let current = getCurrentSelection()
-    toggleHide(current)
     let isCorrect=verifyAnswer()
+    console.log(isCorrect)
     if (!isCorrect) {
         if(seconds-5<0){
             seconds=0
@@ -54,14 +57,16 @@ function nextQuestion(event){
         }
     }
     curentSelection +=1
-    current=getCurrentSelection()
-    toggleHide(current)
+    populateQuestion()
+    if (curentSelection=questionsArray.length){
+    console('stop everything')
+    }
 } 
 function verifyAnswer(){
     let answer
-    let radios=document.getElementsByName('q'+curentSelection)
+    let radios=document.getElementsByName('q0')
     radios.forEach((r)=>{if(r.checked)answer=r.value})
-    if (answer==answers[curentSelection]){
+    if (answer==questionsArray[curentSelection].answer){
         return true
     }
     return false
@@ -77,70 +82,70 @@ function countdown() {
         setTimeout(tick, 1000);
       } else {
         document.getElementById("timer").innerHTML = "";
-        let current=getCurrentSelection()
+        let current=getcurentSelection()
         toggleHide(current)
       }
     }
     tick();
   }
 
-  
+
 
 const questionsArray = [
     {
-        question: '1. Which house was Harry Potter almost sorted into?',
-        option1: 'A',
-        option2: 'B',
-        option3: 'Slytherin',
-        option4: 'D',
-        answer: 3,
+        question: '1. Which house sintaksjdf;ajd;f was Harry Potter almost sorted into?',
+        option1: 'A jsut om',
+        option2: 'B AHDJSDEJ',
+        option3: 'C Slytherin',
+        option4: 'D 12345467',
+        answer: 'C',
     },
     {
         question: '2. Which country gifted the Statue of Liberty to the US?',
         option1: 'A',
         option2: 'B',
         option3: 'C',
-        option4: 'France',
-        answer: 4,
+        option4: 'D France',
+        answer: 'D',
     },
     {
         question: '3. What was the name of the Robin Williams film where he dresses up as an elderly British nanny?',
         option1: 'A',
         option2: 'B',
-        option3: 'Mrs. Doubtfire',
+        option3: 'C Mrs. Doubtfire',
         option4: 'D',
-        answer: 3,
+        answer: 'C',
     },
     {
         question: '4. What is the rarest blood type?',
-        option1: 'AB-Negative',
+        option1: 'A AB-Negative',
         option2: 'B',
         option3: 'C',
         option4: 'D',
-        answer: 1,
+        answer: 'A',
     },
     {
         question: '5. What sport does Cristiano Ronaldo play?',
         option1: 'A',
         option2: 'B',
-        option3: 'Soccer/football',
+        option3: 'C Soccer/football',
         option4: 'D',
-        answer: 3,
+        answer: 'C',
     },
     {
         question: '6. How many bones are there in the human body?',
         option1: 'A',
-        option2: '206',
+        option2: 'B 206',
         option3: 'C',
         option4: 'D',
-        answer: 2,
+        answer: 'B',
     },
     {
         question: '7. What is the name of the longest river in South America?',
         option1: 'A',
-        option2: 'Amazon River',
+        option2: 'B Amazon River',
         option3: 'C',
         option4: 'D',
-        answer: 2,
+        answer: 'B',
     } 
 ];
